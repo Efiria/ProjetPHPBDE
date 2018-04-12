234eeb03412be5fc9df746fdf191ca3d8c9c0023
Efiria <bruno.doucet@viacesi.fr>
2018-04-12 16:01:51 +0200

    supprimer

--- suppProduit.php
+++ suppProduit.php
@@ -1,8 +1,10 @@
 <?php
     $bdd = new PDO('mysql:host=localhost;dbname=projet_test_bdd;charset=utf8', 'root', '');
 
-    $nomProd = $_POST['nomProds'];
-    $requete = $bdd->query("DELETE FROM produits WHERE $nomProd = nom");
+    $nomProd = $_POST['nomProd'];
+
+    $requete = $bdd->prepare("DELETE FROM produits WHERE nom = '$nomProd' ");
+    $requete->execute();
     
     header("Location: shop.php");
 
