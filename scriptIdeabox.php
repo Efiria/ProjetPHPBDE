99179885a2fb3f4e57d25fe71602603f3ada2872
Efiria <bruno.doucet@viacesi.fr>
2018-04-12 10:40:26 +0200

    change ideaBox

--- scriptIdeabox.php
+++ scriptIdeabox.php
@@ -1,20 +1,26 @@
 <?php
 
-include 'include/header.php';
+include 'includes/header.php';
 
-//A changer en fonction
 $bdd = new PDO('mysql:host=localhost;dbname=projet_test_bdd;charset=utf8', 'root', '');
 
-$categorie = $_POST['categorie'];
-$eventdate = $_POST['eventdate'];
+$nom_event = $_POST['nom_event'];
+$date_event = $_POST['date_event'];
 $description = $_POST['description'];
+$status = $_SESSION['status'];
 
-$event_insert = $bdd->prepare("INSERT INTO bai (categorie, eventdate, description) VALUES ( :categorie, :eventdate, :description)");
-$event_insert->bindValue(':categorie', $categorie, PDO::PARAM_STR);
-$event_insert->bindValue(':eventdate', $eventdate, PDO::PARAM_STR);
+if($status=true){
+$event_insert = $bdd->prepare("INSERT INTO bai (nom_event, date_event, description, status) VALUES ( :nom_event, :date_event, :description, :status)");
+$event_insert->bindValue(':nom_event', $nom_event, PDO::PARAM_STR);
+$event_insert->bindValue(':date_event', $date_event, PDO::PARAM_STR);
 $event_insert->bindValue(':description', $description, PDO::PARAM_STR);
-$event_insert->execute() or die('pb insert');
+$event_insert->bindValue(':status', $_SESSION['status'], PDO::PARAM_STR);
 
-echo "test reussi";
+$event_insert->execute() or die('pb insert');
+}
+
+else{
+	echo "Veuillez vous connecter !";
+}
 
 ?>
