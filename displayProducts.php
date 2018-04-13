9b1eb15db81bd808a5bb3b9e2638cb9113e468f8
Efiria <bruno.doucet@viacesi.fr>
2018-04-13 10:14:26 +0200

    recherche categorie

--- displayProducts.php
+++ displayProducts.php
@@ -3,12 +3,37 @@
 
 <?php
 
+//Get product from bdd
+
 $bdd = new PDO('mysql:host=localhost; dbname=projet_test_bdd; charset=utf8', 'root','');
 
-$products = $bdd->prepare('SELECT * FROM produits');
-$products -> execute();
-
 $panier_count = 0;
+
+
+//Search zone
+
+?>
+
+<form action="shop.php" method="POST">
+    <input type="text" name="query" placeholder="<?php $search ?>"/>
+    <input type="submit" value="Search" />
+</form>
+</br>
+
+<?php
+if(isset($_POST['query'])){
+    $search = $_POST['query'];
+    
+    $products = $bdd->prepare("SELECT * FROM produits WHERE categorie = '$search' ");
+    $products -> execute();
+    
+}else {
+    $products = $bdd->prepare('SELECT * FROM produits');
+    $products -> execute();
+}
+
+
+//Create panier
 
 if (isset($_GET["addProduct"]))
 {
@@ -23,6 +48,10 @@ if (isset($_SESSION["panier"]))
 {
 	$panier_count = sizeof($_SESSION["panier"]);
 }
+
+
+
+//Display Product 
 
 while ($answer = $products->fetch()){
 	echo "<div class='displayprod'>"; ?>
@@ -43,55 +72,59 @@ $products -> closeCursor();
 
 if (isset($_SESSION['status']) && $_SESSION['status'] >= 1 ){
     
-?>
+    ?>
 
-<div id="ajouter">
-    <form method="post" action="ajoutProduit.php" autocomplete="on">
-        <h1>Ajouter un Produit</h1> 
-        <p> 
-	     <label for="nomProduit" class="nomProduct" data-icon="u" >Nom du produit : </label>
-	     <input id="nomProduit" name="nomProd" required="required" type="text" placeholder=" Nom "/>
-        </p>
-        
-        <p> 
-	     <label for="prixProd" class="prixProd" data-icon="u" >Prix du produit : </label>
-	     <input id="prixProd" name="prixProd" required="required" type="text" placeholder=" Prix "/>
-        </p>
+    <!-- Ajouter -->
 
-        <p> 
-        <label for="descProd" class="descProd" data-icon="p">Description produit : </label> </br>
-        <textarea id="descProd" name="descProd" required="required" cols="50" rows="10"></textarea>
-        </p>
-        
-        <p> 
-        <label for="cateProd" class="cateProd" data-icon="p">Catégorie du produit : </label>
-        <input id="cateProd" name="cateProd" required="required" type="text" placeholder=" catégorie " /> 
-        </p>
+    <div id="ajouter">
+        <form method="post" action="ajoutProduit.php" autocomplete="on">
+            <h1>Ajouter un Produit</h1> 
+            <p> 
+             <label for="nomProduit" class="nomProduct" data-icon="u" >Nom du produit : </label>
+             <input id="nomProduit" name="nomProd" required="required" type="text" placeholder=" Nom "/>
+            </p>
 
-        <p>
-        <label for="imgProd" class="imgProd" data-icon="p">Image du produit : </label>
-        <input type="file" name="imgProd" />
-        </p>
-                            
-        <p class="ajouter button"> 
-        <input type="submit" value="Ajouter" /> 
-        </p>
-    </form>
-</div>
+            <p> 
+             <label for="prixProd" class="prixProd" data-icon="u" >Prix du produit : </label>
+             <input id="prixProd" name="prixProd" required="required" type="text" placeholder=" Prix "/>
+            </p>
 
-<div id="supprimer">
-    <form method="post" action="suppProduit.php" autocomplete="on">                         
-        <h1>Supprimer un Produit</h1> 
-        <p> 
-	    <label for="nomProds" class="nomProds" data-icon="u" >Nom du Produit: </label>
-	    <input id="nomProd" name="nomProd" required="required" type="text" placeholder="nom"/>
-        </p>
-        
-        <p class="supprimer button"> 
-        <input type="submit" value="Supprimer" /> 
-        </p>
+            <p> 
+            <label for="descProd" class="descProd" data-icon="p">Description produit : </label> </br>
+            <textarea id="descProd" name="descProd" required="required" cols="50" rows="10"></textarea>
+            </p>
 
-    </form>
-</div>
+            <p> 
+            <label for="cateProd" class="cateProd" data-icon="p">Catégorie du produit : </label>
+            <input id="cateProd" name="cateProd" required="required" type="text" placeholder=" catégorie " /> 
+            </p>
+
+            <p>
+            <label for="imgProd" class="imgProd" data-icon="p">Image du produit : </label>
+            <input type="file" name="imgProd" />
+            </p>
+
+            <p class="ajouter button"> 
+            <input type="submit" value="Ajouter" /> 
+            </p>
+        </form>
+    </div>
+
+    <!-- Supprimer -->
+
+    <div id="supprimer">
+        <form method="post" action="suppProduit.php" autocomplete="on">                         
+            <h1>Supprimer un Produit</h1> 
+            <p> 
+            <label for="nomProds" class="nomProds" data-icon="u" >Nom du Produit: </label>
+            <input id="nomProd" name="nomProd" required="required" type="text" placeholder="nom"/>
+            </p>
+
+            <p class="supprimer button"> 
+            <input type="submit" value="Supprimer" /> 
+            </p>
+
+        </form>
+    </div>
 
 <?php } ?>
